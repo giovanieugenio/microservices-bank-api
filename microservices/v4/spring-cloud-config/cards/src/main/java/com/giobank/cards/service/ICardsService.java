798121bdf8ff9260670a02489f0f890ae78ba77f
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-09-19 12:03:30 -0300

    cloud config, profiles...

---
+++ microservices/v4/spring-cloud-config/cards/src/main/java/com/giobank/cards/service/ICardsService.java
@@ -0,0 +1,16 @@
+package com.giobank.cards.service;
+
+
+import com.giobank.cards.dto.CardsDto;
+
+public interface ICardsService {
+
+    void createCard(String mobileNumber);
+
+    CardsDto fetchCard(String mobileNumber);
+
+    boolean updateCard(CardsDto cardsDto);
+
+    boolean deleteCard(String mobileNumber);
+
+}
---
+++ microservices/v4/spring-cloud-config/loans/src/main/java/com/giobank/loans/LoansApplication.java
@@ -0,0 +1,43 @@
+package com.giobank.loans;
+
+import com.giobank.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST Api documentation",
+				description = "GioBank Loans microservice REST Api Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Giovani Eugenio",
+						email = "giovani.eugenio.sp@gmail.com",
+						url = "https://www.giobank.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.giobank.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "GioBank Loans microservice REST Api Documentation",
+				url = "https://www.giobank.com/swagger-ui/index.html"
+		)
+)
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = LoansContactInfoDto.class)
+@SpringBootApplication
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
