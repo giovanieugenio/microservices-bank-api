81249c1d5c7cef8177a1740e1ce0b73783dc5335
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-09-19 19:46:08 -0300

    replaced h2 by mysql

---
+++ microservices/v5/accounts/src/main/java/com/giobank/accounts/dto/ErrorResponseDto.java
@@ -0,0 +1,25 @@
+package com.giobank.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Schema(
+        name = "Response",
+        description = "schema to hold error response information"
+)
+@AllArgsConstructor
+@Data
+public class ErrorResponseDto {
+
+    private String apiPath;
+
+    private HttpStatus errorCode;
+
+    private String errorMessage;
+
+    private LocalDateTime errorTime;
+}
---
+++ microservices/v5/accounts/src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id int auto_increment primary key,
+    name varchar(100) not null,
+    email varchar(100) not null,
+    mobile_number varchar(14) not null,
+    created_at date not null,
+    created_by varchar(30) not null,
+    updated_at date default null,
+    updated_by varchar(30) default null
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+    customer_id int not null,
+    account_number int auto_increment primary key,
+    account_type varchar(100) not null,
+    branch_address varchar(200) not null,
+    created_at date not null,
+    created_by varchar(30) not null,
+    updated_at date default null,
+    updated_by varchar(30) default null
+);
